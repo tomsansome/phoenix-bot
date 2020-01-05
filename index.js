953aedf9fe108324d60a431bb67197699c5c3a4c
Tom Sansome <sansometom@gmail.com>
2020-01-05 11:58:59 +0000

    Clean up

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-const fs = require('fs')
 const Koa = require('koa')
 const app = new Koa()
 const getFilmTimes = require('./src/getFilmTimes')
-const Cron = require('cron').CronJob
-const everySec = '10 * * * * *'
-const everyMin = '* * * * *'
-const everyDay = '0 0 * * *'
-
-// new Cron(everySec, async () => { writeToDisc() }, null, true)
 
 app.use(async (ctx) => {
-  // Read data file
-  // try {
-  //   fs.readFileSync('data.json', 'utf8')
-  // } catch (err) {
-  //   console.error(err)
-  // }
-
-  // Parse file to then give to context body
-  // const result = JSON.parse(file)
-
   const result = await getFilmTimes()
-
-  // Respond with result
   ctx.body = result
 })
 
--- thing.test.js
+++
@@ -1,5 +0,0 @@
-import test from 'ava'
-
-test('Does a thing', async t => {
-  t.pass()
-})
