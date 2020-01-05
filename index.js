a6df48c66b0877a5f4df2c156d891a4ce7128bb0
Tom Sansome <sansometom@gmail.com>
2020-01-05 11:54:32 +0000

    No cron on now :(

--- index.js
+++ index.js
@@ -7,18 +7,20 @@ const everySec = '10 * * * * *'
 const everyMin = '* * * * *'
 const everyDay = '0 0 * * *'
 
-new Cron(everySec, async () => { writeToDisc() }, null, true)
+// new Cron(everySec, async () => { writeToDisc() }, null, true)
 
 app.use(async (ctx) => {
   // Read data file
-  try {
-    fs.readFileSync('data.json', 'utf8')
-  } catch (err) {
-    console.error(err)
-  }
+  // try {
+  //   fs.readFileSync('data.json', 'utf8')
+  // } catch (err) {
+  //   console.error(err)
+  // }
 
   // Parse file to then give to context body
-  const result = JSON.parse(file)
+  // const result = JSON.parse(file)
+
+  const result = await getFilmTimes()
 
   // Respond with result
   ctx.body = result
