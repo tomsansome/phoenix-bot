13a5c2107ebd46cea64c957a05aea03a23368ed3
Tom Sansome <sansometom@gmail.com>
2020-01-05 11:29:45 +0000

    Write result to file to speed things up

--- index.js
+++ index.js
@@ -1,61 +1,40 @@
-const moment = require('moment')
-const fetch = require('node-fetch')
-const { JSDOM } = require('jsdom')
+
+const fs = require('fs')
 const Koa = require('koa')
 const app = new Koa()
+const Cron = require('cron').CronJob
+const getFilmTimes = require('./src/getFilmTimes')
+// const everySecs = '2 * * * * *'
+// const everyMin = '* * * * *'
+const everyDay = '0 0 * * *'
+
+new Cron(everyDay, async () => {
+  // Get films and times
+  const result = await getFilmTimes()
+
+  // Stringify results
+  const json = JSON.stringify(result)
+
+  // Write them all to disc
+  try {
+    fs.writeFileSync('data.json', json, 'utf8', () => { })
+  } catch (err) {
+    console.error(err)
+  }
+}, null, true)
 
 app.use(async (ctx) => {
-  let result = []
-
-  const url = 'https://www.phoenix.org.uk/whats-on/?category=cinema&view=list'
-
-  const fetchTextAndDom = async url => {
-    const data = await fetch(url).then(r => r.text())
-    return new JSDOM(data).window.document
+  // Read data file
+  try {
+    fs.readFileSync('data.json', 'utf8')
+  } catch (err) {
+    console.error(err)
   }
 
-  const doc = await fetchTextAndDom(url)
+  // Parse file to then give to context body
+  const result = JSON.parse(file)
 
-  const days = doc.querySelectorAll('.blockheading')
-  const daysArray = [...days]
-  daysArray.splice(0, 1)
-  daysArray.splice(7, 9)
-
-  let i = 0
-
-  const getFilmsForDay = day => {
-    let result = []
-
-    const sections = doc.querySelectorAll('.listing--whats-on')
-    const sectionsArray = [...sections]
-
-    const films = sectionsArray[day].querySelectorAll('.listing__item')
-
-    for (const film of films) {
-      const filmTitle = film.querySelector('.media__title a').textContent.trim()
-      const cleanTitle = filmTitle.split('/')[1].trim()
-
-      const times = film.querySelectorAll('.listing__showing')
-
-      let timesResult = []
-      for (const time of times) {
-        timesResult.push(time.querySelector('.button').textContent.trim())
-      }
-
-      result.push({ film: cleanTitle, times: timesResult })
-    }
-
-    return result
-  }
-
-  for (const day of daysArray) {
-    const date = day.textContent
-    const dateFormatted = moment(date).format('DDMMYYYY')
-    const films = getFilmsForDay(i)
-    result.push({ date, dateFormatted, films })
-    i++
-  }
-
+  // Respond with result
   ctx.body = result
 })
 
